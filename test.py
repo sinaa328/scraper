2e8c4c83cbfcb9d51508c85b9a03ab26fc40eda3
sina <sinaa328@gmail.com>
2023-04-06 04:45:47 +0330

    begaii

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import requests
 import base64
 import json
+import os
+import wget
 
 url = "https://avaye-ryra.ir/wp-json/wp/v2/posts"
 
@@ -9,10 +11,24 @@ token = base64.b64encode(creditionals.encode()).decode()
 
 header = {'Authorization': f'Basic {token}'}
 
+babo = wget.download('https://cdn.borna.news/thumbnail/jGQcWuuSTlYf/zKlnR2CgDMsLPypExSqkWVexFSgSRltNvp3exfX06vug6WJOtspsxLwVvO9faXc9wvplisndFdUHpxCkrVbVYeF9dj5XguJb/74.jpg')
+
+# pic = os.rename('74.jpg','image.jpg')
+
+media = {
+    'file' : open('74.jpg', 'rb'),
+    'caption' : 'try try'
+}
+
+image = requests.post(url + '/media', headers=header, files=media)
+imageURL = str(json.loads(image.content))
+print(imageURL)
+
+
 post = {
     'date': '2023-04-04T21:00:00',
     'title': 'first try',
-    'content': 'kosshere mahz for try',
+    'content': 'kosshere mahz for try <img src="' + imageURL + '">'
 }
 
 r = requests.post(url, headers=header, data=post)
